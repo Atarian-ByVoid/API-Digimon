f0ef78926d5440926c1ce206380d1dea7386ba53
Atarian <cgomespimentel7@gmail.com>
2023-02-19 09:20:20 -0300

    New GN8

--- src/main/java/com/planotatico/digimon/digimon/DigimonApplication.java
+++ src/main/java/com/planotatico/digimon/digimon/DigimonApplication.java
@@ -17,40 +17,10 @@ public class DigimonApplication {
 		DigimonClient digimonClient = new DigimonClient();
 		List<Digimon> listaDigimon = digimonClient.listar();
 
-		/*
-		 * Foi usado um Scanner para que o usuario possa digitar o level do Digimon,
-		 * para realizar a rapida consulta!
-		 */
-		System.out.print(
-				"Level disponiveis para consulta ------ In Training | Rookie | Champion | Ultimate | Mega | Fresh ");
-
-		try (Scanner scanner = new Scanner(System.in)) {
-			String resposta = scanner.nextLine();
-
-			System.out.print("Escolha o level do Digimon: ");
-			resposta = scanner.next();
-
-			/*
-			 * Aqui onde haverá um retorno de uma lista de level dos Digimons, com base no
-			 * que foi escolhido pelo usuario !
-			 */
-			for (Digimon digimon : listaDigimon) {
-
-				if (digimon.getLevel().equals(resposta)) {
-
-					System.out.print("--" + digimon.getName());
-					System.out.println(" Lvl." + digimon.getLevel());
-
-				}
-
-			}
-			System.out.println("---------------!!SUA BUSCA TERMINA AQUI!!---------------");
-			System.out.println();
-
+		
 			/* Será listado a seguir todos os digimons */
 
-			System.out
-					.println("______________________LISTA DE TODOS OS DIGIMONS____________________________");
+			System.out.println("______________________LISTA DE TODOS OS DIGIMONS____________________________");
 			System.out.println();
 
 			for (Digimon digimon : listaDigimon) {
@@ -59,6 +29,35 @@ public class DigimonApplication {
 				System.out.println(" ___ Lvl." + digimon.getLevel());
 
 			}
+			System.out.println();
+			
+		/*
+		 * Foi usado um Scanner para que o usuario possa digitar o level do Digimon,
+		 * para realizar a rapida consulta!
+		 */
+		System.out.print("Level disponiveis para consulta --- In Training | Rookie | Champion | Ultimate | Mega | Fresh --- Pressione ENTER para continuar");
+
+	try (Scanner scanner = new Scanner(System.in)) {
+		String resposta = scanner.nextLine();
+
+		System.out.print("Escolha o level do Digimon: ");
+		resposta = scanner.next();
+
+		/*
+		 * Aqui onde haverá um retorno de uma lista de level dos Digimons, com base no
+		 * que foi escolhido pelo usuario !
+		 */
+		for (Digimon digimon : listaDigimon) {
+
+			if (digimon.getLevel().equals(resposta)) {
+
+				System.out.print("--" + digimon.getName());
+				System.out.println(" Lvl." + digimon.getLevel());
+
+			}
+
+		}
+		System.out.println("---------------!!SUA BUSCA TERMINA AQUI!!---------------");
 
 		}
 
